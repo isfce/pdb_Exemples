2595c285bcfcee5fbc52b074cf65ccc6396d0561
VOZ <voisfce@gmail.com>
2023-02-15 20:30:17 +0100

    Lombok et JavaFX sans le 11

---
+++ pdbExemples/src/exemples/java/TestLombok.java
@@ -0,0 +1,28 @@
+package exemples.java;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+public class TestLombok {
+
+	private final int id;
+	private String nom;
+
+	/**
+	 * @param id
+	 * @param nom
+	 */
+	public TestLombok(int id, String nom) {
+		this.id = id;
+		this.nom = nom;
+		log.info("Une Test est créé!!");
+	}
+
+	public static void main(String[] args) {
+		TestLombok t1 = new TestLombok(1, "test1");
+		System.out.println(t1);
+	}
+
+}
